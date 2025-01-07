6859260947aa9cd9bdc4bbaecbf2638dcf3dc9dd
ThiQuynh04 <Quynhthi17052004@gmail.com>
2025-01-07 22:25:59 +0700

    add project

--- TH02_QuynhVT/TH02BT_QuynhVT/b13.py
+++ TH02_QuynhVT/TH02BT_QuynhVT/b13.py
@@ -1,4 +1,10 @@
 # Bài 13: Đổi số từ thập phân sang nhị phân
-n = int(input("Nhập số tự nhiên N (N > 0): "))
-binary = bin(n)[2:]
-print(f"Hệ nhị phân của {n}: {binary}")
+try:
+    n = int(input("Nhập số tự nhiên N (N > 0): "))
+    if n > 0:
+        binary = bin(n)[2:]
+        print(f"Hệ nhị phân của {n}: {binary}")
+    else:
+        print("Vui lòng nhập số lớn hơn 0.")
+except ValueError:
+    print("Vui lòng nhập một số hợp lệ;.")
